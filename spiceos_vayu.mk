b43b46f049829506135ce3b0d5cc015aded02dc3
jijinraj <jijinraj401@gmail.com>
2022-07-11 08:44:47 +0530

    somchange dk

--- spiceos_vayu.mk
+++ spiceos_vayu.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/spiceos/config/common.mk)
+$(call inherit-product, vendor/spiceos/config/common_full_phone.mk)
 
 # Inherit from custom vendor
-$(call inherit-product, vendor/miuicamera/config.mk)
+#$(call inherit-product, vendor/miuicamera/config.mk)
 
 PRODUCT_NAME := spiceos_vayu
 PRODUCT_DEVICE := vayu
