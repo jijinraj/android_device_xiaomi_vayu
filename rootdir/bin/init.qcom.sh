a6a77b1cd4728d69f709ca092af849e2fb71d8b0
jijinraj <jijinraj401@gmail.com>
2022-09-06 20:46:35 +0530

    nevermind , wip

--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -463,3 +463,6 @@ case "$buildvariant" in
         echo "4 4 1 4" > /proc/sys/kernel/printk
         ;;
 esac
+
+# Remove settings cache, avoids derps after dirty flash (gonna work ?)
+rm -rf /data/system/package_cache
